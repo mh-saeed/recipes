04cfa2c4bc30c9bb9991ed19b083c625a2a21e32
mh-saeed <saeedkhan111244@gmail.com>
2021-11-14 13:24:17 +0500

    error page completed

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,12 @@ import Layout from "../component/Layout"
 const Error = () => {
   return (
     <Layout>
-      <h1>Error</h1>
+      <main className="error-page">
+        <section>
+          <h1>404</h1>
+          <h3>page not found</h3>
+        </section>
+      </main>
     </Layout>
   )
 }
