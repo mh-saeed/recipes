3031a81b77b0ca331e2e2cf1cbce1d7577c56a92
mh-saeed <saeedkhan111244@gmail.com>
2021-11-17 23:25:00 +0500

    about page basic work done

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,35 @@
 import React from "react"
 import Layout from "../component/Layout"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const About = () => {
   return (
     <Layout>
-      <h1>About</h1>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>I'm baby coloring book poke taxidermy</h2>
+            <p>
+              Taxidermy forage glossier letterpress heirloom before they sold
+              out you probably haven't heard of them banh mi biodiesel chia.
+            </p>
+            <p>
+              Taiyaki tumblr flexitarian jean shorts brunch, aesthetic salvia
+              retro.
+            </p>
+            <Link to="/contact" className="btn">
+              Contact
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="Person Pouring Salt"
+            className="about-img"
+            placeholder="blurred"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
