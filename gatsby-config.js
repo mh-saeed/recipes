9152c99c1ffd4221e0a7ca1164115ff1df0bf8c3
mh-saeed <saeedkhan111244@gmail.com>
2022-01-05 20:05:19 +0500

    updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,12 +4,43 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Saeed's recipes`,
+    description: "Simple and Easy Recipes",
+    author: "@Anonymous",
+    person: { name: "saeed", age: "21" },
+    simpleData: ["item1", "item2", "item3"],
+    complexData: [
+      { name: "saeed", age: "21" },
+      { name: "john", age: "32" },
+      { name: "mickle", age: "27", status: "single" },
+    ],
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `dusedjw0u88v`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
+      },
+    },
   ],
 }
