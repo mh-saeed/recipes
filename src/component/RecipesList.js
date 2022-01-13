508da26c49e0ba781fae1aaff1f177452911555a
mh-saeed <saeedkhan111244@gmail.com>
2022-01-13 12:38:35 +0500

    updated

--- src/component/RecipesList.js
+++ src/component/RecipesList.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import slugify from "slugify"
 
 const RecipesList = ({ recipes = [] }) => {
   return (
@@ -9,8 +10,9 @@ const RecipesList = ({ recipes = [] }) => {
         const { title, image, prepTime, cookTime } = recipe
 
         const pathToImage = getImage(image)
+        const slug = slugify(title, { lower: true })
         return (
-          <Link key={title} to={`/${title}`} className="recipe">
+          <Link key={title} to={`/${slug}`} className="recipe">
             <GatsbyImage
               image={pathToImage}
               className="recipe-img"
---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const RecipeTemplate = props => {
+  console.log(props.pageContext.title)
+  return <div>Recipe Template</div>
+}
+
+export default RecipeTemplate
